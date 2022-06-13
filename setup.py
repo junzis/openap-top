d751768bde085d59a707f42d8acdf5f7e22081c1
Junzi Sun <junzisun@gmail.com>
2022-06-13 17:21:52 +0200

    update version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="openap-top",
-    version="1.0",
+    version="1.1",
     description="OpenAP Trajectory OPtimizer",
     long_description=long_description,
     long_description_content_type="text/markdown",
